40b94304e372bc243706e9283b13ad6d8c832870
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-13 00:46:01 +0900

    imprement events_index2

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -11,13 +11,18 @@ class EventsIndex extends Component{
     }
     
     renderEvents() {
-    return _.map(this.props.events, event => (
+        const evs = this.props.events;
+        /*
+        console.log('events_index');
+        console.log( evs );
+        */
+    return _.map(evs, event => (
       <tr key={event.id}>
         <td>{event.id}</td>
         <td>{event.title}</td>
         <td>{event.body}</td>
       </tr>
-    ))
+    ));
     }
     
     render() {
