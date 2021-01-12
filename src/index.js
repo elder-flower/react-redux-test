2afb3193b8ed69b67836165a66cd8c1396680e14
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-13 01:38:16 +0900

    imprement events_new

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './index.css';
-import reducer from './reducers/';
+import reducer from './reducers'
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 
-const store = createStore(reducer, applyMiddleware(thunk) );
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <Provider store={store}>
-        <EventsIndex />
-    </ Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -4,10 +4,12 @@ import { READ_EVENTS } from '../actions';
 export default( events = {}, action ) => {
     switch( action.type ){
         case READ_EVENTS:
+            /*
             console.log('events action');
-            let aaa = _.mapKeys(action.response.data, 'id')
+            let aaa = _.mapKeys(action.response.data, 'id');
             console.log(action);
             console.log(aaa);
+            */
             return _.mapKeys(action.response.data, 'id');
         default:
             return events
