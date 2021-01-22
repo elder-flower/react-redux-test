a083e92db24293d37b9bac192fe95a9091841f96
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-22 22:06:23 +0900

    redux-form (1) - Simple Form Example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Navbar from './Navbar';
-import About from './About';
-import Home from './Home';
+import SimpleForm from './SimpleForm';
+import './index.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-            <Switch>
-              <>
-                <Navbar /><hr/>
-                <Route exact path="/" component={Home} />
-                <Route path="/About" component={About} />
-              </>
-            </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
+class App extends Component{
+    constructor(props) {
+        super(props)
+        this.showResults = this.showResults.bind(this)
+    }
+    async showResults(values){
+        return new Promise(resolve => {
+            setTimeout(() => {
+              // simulate server latency
+              window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
+              resolve()
+            }, 500);
+        });
+    }
+
+
+    render() {
+        return (
+            <>
+                <h2>Form</h2>
+                <SimpleForm onSubmit={this.showResults} />
+            </>
+        );
+    }
 }
 
 export default App;
