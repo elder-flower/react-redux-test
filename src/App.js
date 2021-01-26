913e18cdeb0c44ae63b14153637be70dd34e7457
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-26 22:42:45 +0900

    コールバック関数を Props として受け取ったコンポーネントは必ず再レンダリングされる

--- src/App.js
+++ src/App.js
@@ -1,55 +1,28 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import './index.css';
 
-const Child = React.memo(() => {
+const Child = React.memo(props => {
   console.log("render Child");
-  return <p>Child</p>;
+  return <button onClick={props.handleClick}>Child</button>;
 });
 
 export default function App() {
   console.log("render App");
 
-  const [timeLeft, setTimeLeft] = useState(100);
-  /*
-    setIntervalの「intervalID」を保持する。
-  */
-  const IntervalID = useRef(null);
-  /*
-    「useRef()」生成時の第1引数は useRef(initialValue);
-  */
-  const timeLeftRef = useRef(timeLeft);
-
-　/*
-    毎回副作用が更新されるのを第2引数に対象プロパティを設定する事によってそのプロパティが変更された時のみ更新する。様に変更する。
- */
-  useEffect(() => {
-    timeLeftRef.current = timeLeft;
-  }, [timeLeft]);
-
-  const tick = () => {
-    if (timeLeftRef.current === 0) {
-      clearInterval(IntervalID.current);
-      return;
-    }
-    setTimeLeft(prevTime => prevTime - 1);
-  };
-
-  const start = () => {
-    IntervalID.current = setInterval(tick, 10);
-  };
-
-  const reset = () => {
-    clearInterval(IntervalID.current);
-    setTimeLeft(100);
+  const [count, setCount] = useState(0);
+  // 関数はコンポーネントが再レンダリングされる度に再生成されるため、
+  // 関数の内容が同じでも、新しい handleClick と前回の handleClick は
+  // 異なるオブジェクトなので、等価ではない。
+  // そのため、コンポーネントが再レンダリングされる。
+  const handleClick = () => {
+    console.log("click");
   };
 
   return (
     <>
-      <button onClick={start}>start</button>
-      <button onClick={reset}>reset</button>
-      <p>App: {timeLeft}</p>
-      <Child />
+      <p>Counter: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment count</button>
+      <Child handleClick={handleClick} />
     </>
   );
 }
-
