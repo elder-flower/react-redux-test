3fe25b553d0b4f624eb39f69b4e667edbfdc146d
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-27 01:56:59 +0900

    useCallback でメモ化したコールバック関数を、それを生成したコンポーネント自身で利用する

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { useState, useCallback } from "react";
 import './index.css';
 
-const Child = React.memo(props => {
-  console.log("render Child");
-  return <button onClick={props.handleClick}>Child</button>;
-});
-
 export default function App() {
   console.log("render App");
 
   const [count, setCount] = useState(0);
-  // 関数をメモ化すれば、新しい handleClick と前回の handleClick は
-  // 等価になる。そのため、Child コンポーネントは再レンダリングされない。
   const handleClick = useCallback(() => {
-    console.log("click");
+    console.log("memonized callback");
   }, []);
 
   return (
     <>
       <p>Counter: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment count</button>
-      <Child handleClick={handleClick} />
+      <button onClick={handleClick}>logging</button>
     </>
   );
 }
