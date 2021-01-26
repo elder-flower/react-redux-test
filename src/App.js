a33ca7b7461631217c5e9bde4b1e437b263f9563
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-27 02:00:36 +0900

    useMemo を利用しない場合

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import './index.css';
 
 export default function App() {
-  console.log("render App");
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-  const [count, setCount] = useState(0);
-  const handleClick = useCallback(() => {
-    console.log("memonized callback");
-  }, []);
+  // 引数の数値を２倍にして返す。
+  // 不要なループを実行しているため計算にかなりの時間がかかる。
+  const double = count => {
+    console.log('func double');
+    let i = 0;
+    while (i < 1000000000) i++;
+    return count * 2;
+  };
+
+  // count2 を２倍にした値
+  // double(count2) はコンポーネントが再レンダリングされる度に実行されるため、
+  // count1 を更新してコンポーネントが再レンダリングされた時にも実行されてしまう。
+  // そのため、count1 を更新してコンポーネントを再レンダリングする時も時間がかかる。
+  // count1 を更新しても doubledCount の値は変わらないため、count1 を更新した時に
+  // double(count2) を実行する意味がない。したがって、不要な再計算が発生している状態である。
+  // count1 が更新されてコンポーネントが再レンダリングされた時は double(count2) が実行されないようにしたい。
+  const doubledCount = double(count2);
 
   return (
     <>
-      <p>Counter: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment count</button>
-      <button onClick={handleClick}>logging</button>
+      <h2>Increment count1</h2>
+      <p>Counter: {count1}</p>
+      <button onClick={() => setCount1(count1 + 1)}>Increment count1</button>
+
+      <h2>Increment count2</h2>
+      <p>
+        Counter: {count2}, {doubledCount}
+      </p>
+      <button onClick={() => setCount2(count2 + 1)}>Increment count2</button>
     </>
   );
 }
