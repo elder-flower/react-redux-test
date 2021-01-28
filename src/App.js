642a6929db6ccb200d0d79dc446c93b127079cb2
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-29 08:32:26 +0900

    02-useEffect で副作用を１度だけ実行させる（外部 API からデータを取得する）2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React, { useState, useEffect } from "react";
+// HTTP 通信をするモジュール（外部 API などと通信する時に利用する）
+import axios from "axios";
 import './index.css';
 
-export default function App() {
-  const [count, setCount] = useState(0);
 
-  // <p id="effectHook"></p> から取得したテキストをコンソールに出力する副作用。
-  // 今回の場合、App コンポーネントが再レンダーされる度に実行される。
-  // 副作用はコンポーネントのレンダー後に実行されるため、App コンポーネントの
-  // レンダーで生成された <p id="effectHook"></p> も操作できる。
-  // コンポーネントは state が更新される度にレンダーされるため、App コンポーネントは
-  // count が更新される度に再レンダーされる。そのため、ボタンをクリックする度に
-  // App コンポーネントは再レンダーされ、この副作用も実行される。
+
+export default function App() {
+  // API から取得したデータ
+  const [items, setItems] = useState([]);
+  // ローディング状態
+  const [isLoading, setIsLoading] = useState(false);
+  /*
+  console.log('isLoading');
+  console.log( isLoading );
+  */
+
+  // コンポーネントが初めてレンダーされた後に外部 API からデータを取得し、state を更新する副作用。
+  // useEffect の第２引数（今回は空の配列）を指定しないと、items や isLoading が更新され、
+  // コンポーネントが再レンダーされる度に API との通信が発生してしまう。
+  // 今回はコンポーネントがレンダーされた後に、１度だけこの処理を実行したいので、第２引数に [] を渡している。
   useEffect(() => {
-    console.log(document.getElementById("effectHook").innerText);
-  });
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      const result = await axios(
+        "https://hn.algolia.com/api/v1/search?query=react"
+      );
+
+      setItems(result.data.hits);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      <p id="effectHook">You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>click</button>
-    </div>
+    <>
+      {isLoading ? (
+        <p>Loading</p>
+      ) : (
+        <ul>
+          {items.map((item) => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
