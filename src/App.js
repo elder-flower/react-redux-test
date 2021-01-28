a4b15194dc49c20686cfbc1ba98b0d2a2f22b72a
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-28 23:07:13 +0900

    04-usestate でオブジェクトを扱っているがコンポーネントが再レンダーされない例

--- src/App.js
+++ src/App.js
@@ -5,23 +5,17 @@ export default function App() {
   const [vote, setVote] = useState({ kinoko: 0, takenoko: 0 });
 
   const voteKinoko = () => {
-    // 現在の vote に、kinoko プロパティ（現在の vote.kinoko + 1）を
-    // マージしたオブジェクトを setVote に渡す。
-    // 例えば、vote が以下の状態であれば
-    // { kinoko: 0, takenoko: 0 }
-    // 以下のように更新される
-    // { kinoko: 1, takenoko: 0 }
-    setVote({ ...vote, kinoko: vote.kinoko + 1 });
+    // 以下のように state を更新してしまうと、React は state が
+    // 更新されていないと判定するのでコンポーネントが再レンダーされない。
+    vote.kinoko = vote.kinoko + 1;
+    setVote(vote);
   };
 
   const voteTakenoko = () => {
-    // 現在の vote に、takenoko プロパティ（現在の vote.takenoko + 1）を
-    // マージしたを setVote に渡す。
-    // 例えば、vote が以下の状態であれば
-    // { kinoko: 0, takenoko: 0 }
-    // 以下のように更新される
-    // { kinoko: 0, takenoko: 1 }
-    setVote({ ...vote, takenoko: vote.takenoko + 1 });
+    // 以下のように state を更新してしまうと、React は state が
+    // 更新されていないと判定するのでコンポーネントが再レンダーされない。
+    vote.takenoko = vote.takenoko + 1;
+    setVote(vote);
   };
 
   return (
