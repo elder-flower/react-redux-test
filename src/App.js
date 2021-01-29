5eac16a3d5c916f57f5c7be525eb2bff5795d887
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-29 21:06:15 +0900

    02-useState を利用した場合、state の更新方法の数だけ、コンポーネントに state 更新関数を渡す必要がある

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
-import React, { useReducer } from 'react';
-import './index.css';
+import React, { useState } from 'react';
 
-// 現在の state と action を受け取り、action に応じて更新した state を返す関数
-function reducer(state, action) {
-  switch (action.type) {
-    case 'INCEREMENT':
-      return { count: state.count + 1 };
-    case 'DECREMENT':
-      return { count: state.count - 1 };
-    case 'RESET':
-      return { count: 0 };
-    default:
-      return state;
-  }
-}
+const Counter = ({ decrement, increment, reset }) => {
+  return (
+    <>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
+      <button onClick={reset}>reset</button>
+    </>
+  );
+};
 
 export default function App() {
-  // useReducer の第２引数に { count: 0 } を渡しているので、
-  // state の初期値は { count: 0 }
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [count, setCount] = useState(0);
+
+  const decrement = () => {
+    setCount(currentCount => currentCount - 1);
+  };
+
+  const increment = () => {
+    setCount(currentCount => currentCount + 1);
+  };
+
+  const reset = () => {
+    setCount(() => 0);
+  };
 
   return (
     <>
-      <p>count: {state.count}</p>
-
-      {/* { type: 'DECREMENT' } という action を送信する */}
-      <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
-
-      {/* { type: 'INCEREMENT' } という action を送信する */}
-      <button onClick={() => dispatch({ type: 'INCEREMENT' })}>+</button>
-
-      {/* { type: 'RESET' } という action を送信する */}
-      <button onClick={() => dispatch({ type: 'RESET' })}>reset</button>
+      <p>Count: {count}</p>
+      <Counter decrement={decrement} increment={increment} reset={reset} />
     </>
   );
 }
