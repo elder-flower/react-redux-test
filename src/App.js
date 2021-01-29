36a29d6816d9d91f151a1645a981addc9a2f9085
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-29 22:36:57 +0900

    03-複数のコンポーネントで共通利用するデータを Context で扱い、それを useContext で取得する

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useContext, createContext } from "react";
 import './index.css';
 
+// Context オブジェクト
+const LangContext = createContext();
+
 export default function App() {
-  const [count, setCount] = useState(0);
-  const value = {
-    name: "soarflat",
-    handleClick: () => setCount((count) => count + 1)
+  const [lang, setLang] = useState("JP");
+
+  return (
+    <>
+      <button onClick={() => setLang("JP")}>日本語</button>
+      <button onClick={() => setLang("EN")}>English</button>
+      <LangContext.Provider value={lang}>
+        <Header />
+        <Body />
+      </LangContext.Provider>
+    </>
+  );
+}
+
+function Header() {
+  const lang = useContext(LangContext);
+  const text = {
+    JP: "速習 React Hooks",
+    EN: "React Hooks Quick Start Guide"
   };
 
   return (
-    <div>
-      <p>count: {count}</p>
-      <Child value={value} />
-    </div>
+    <header>
+      <h1>{text[lang]}</h1>
+    </header>
   );
 }
 
-// Child コンポーネント自体は value を利用しないが、GrandChild コンポーネントに
-// Props として渡すために、value を受け取る必要がある。
-function Child({ value }) {
-  return <GrandChild value={value} />;
-}
+function Body() {
+  const lang = useContext(LangContext);
+  const text = {
+    JP: "useContext の利用例",
+    EN: "useContext Example"
+  };
 
-// GrandChild コンポーネント自体は value を利用しないが、GreatGrandChild コンポーネントに
-// Props として渡すために、value を受け取る必要がある。
-function GrandChild({ value }) {
-  return <GreatGrandChild value={value} />;
-}
-
-function GreatGrandChild({ value }) {
-  return (
-    <>
-      <p>{value.name}</p>
-      <button onClick={value.handleClick}>increment</button>
-    </>
-  );
+  return <p>{text[lang]}</p>;
 }
