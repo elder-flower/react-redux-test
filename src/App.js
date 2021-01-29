0f23b6a6294498625f745ae9d94902b49b9ce5d3
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-29 11:05:34 +0900

    10-useMemo で不要な再計算をスキップする

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import './index.css';
 
 export default function App() {
@@ -13,26 +13,22 @@ export default function App() {
     return count * 2;
   };
 
-  // count2 を２倍にした値
-  // double(count2) はコンポーネントが再レンダーされる度に実行されるため、
-  // count1 を更新してコンポーネントが再レンダーされた時にも実行されてしまう。
-  // そのため、count1 を更新してコンポーネントを再レンダーする時も時間がかかる。
-  // count1 を更新しても doubledCount の値は変わらないため、count1 を更新した時に
-  // double(count2) を実行する意味がない。したがって、不要な再計算が発生している状態である。
-  // count1 が更新されてコンポーネントが再レンダーされた時は double(count2) が実行されないようにしたい。
-  const doubledCount = double(count2);
+  // count2 を２倍にした値をメモ化する。
+  // 第２引数に count2 を渡しているため、count2 が更新された時だけ値が再計算される。
+  // count1 が更新され、コンポーネントが再レンダーされた時はメモ化した値を利用するため再計算されない。
+  const doubledCount = useMemo(() => double(count2), [count2]);
 
   return (
     <>
-      <h2>Increment count1</h2>
+      <h2>Increment(fast)</h2>
       <p>Counter: {count1}</p>
-      <button onClick={() => setCount1(count1 + 1)}>Increment count1</button>
+      <button onClick={() => setCount1(count1 + 1)}>Increment(fast)</button>
 
-      <h2>Increment count2</h2>
+      <h2>Increment(slow)</h2>
       <p>
         Counter: {count2}, {doubledCount}
       </p>
-      <button onClick={() => setCount2(count2 + 1)}>Increment count2</button>
+      <button onClick={() => setCount2(count2 + 1)}>Increment(slow)</button>
     </>
   );
 }
