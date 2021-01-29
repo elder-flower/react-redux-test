e215994fe5322e79906d796cd9b40501791cff75
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-29 10:51:31 +0900

    01-React.memo を利用しない場合、親コンポーネントが再レンダーされると、その子コンポーネントも常に再レンダーされる

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import './index.css';
 
-export default function App() {
-  const inputEl = useRef(null);
-  const onClick = () => {
-    if (!inputEl.current) return;
+// App コンポーネントが再レンダーする度に再レンダーされる
+function Child({ count }) {
+  console.log("render Child");
+  return <p>Child: {count}</p>;
+}
 
-    inputEl.current.focus();
-  };
+export default function App() {
+  console.log("render App");
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
   return (
     <>
-      {/* ref 属性に inputEl を指定することで、inputEl.current で DOM にアクセスできる */}
-      <input ref={inputEl} type="text" />
-      <button onClick={onClick}>input要素をフォーカスする</button>
+      <button onClick={() => setCount1(count1 + 1)}>countup App count</button>
+      <button onClick={() => setCount2(count2 + 1)}>countup Child count</button>
+      <p>App: {count1}</p>
+      <Child count={count2} />
     </>
   );
 }
