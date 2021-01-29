537505ab421a451a0de7474f223c74adf7e802f7
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-29 09:48:58 +0900

    01-useRef で前回の state の値を保持して要素の出し分けに利用する

--- src/App.js
+++ src/App.js
@@ -1,61 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import './index.css';
 
-const LIMIT = 60;
+export default function App() {
+  const [count, setCount] = useState(10);
+  // useRef に　0 を渡しているので、prevCountRef.current の初期値は 0
+  const prevCountRef = useRef(0);
 
-// カウントダウンをする（60 から 0 を表示する）コンポーネント。
-function Timer() {
-  // カウント
-  const [timeLeft, setTimeLeft] = useState(LIMIT);
-
-  // timeLeft をリセット（60に戻す）。
-  const reset = () => {
-    setTimeLeft(LIMIT);
-  };
-
-  // timeLeft を更新する。
-  const tick = () => {
-    console.log("tick");
-    setTimeLeft((prevTime) => (prevTime === 0 ? LIMIT : prevTime - 1));
-  };
-
-  // setInterval で１秒毎に tick を実行するタイマーを作成する副作用。
-  // 第２引数に [] を渡しているので、この副作用はレンダー後の１度しか実行されない。
   useEffect(() => {
-    console.log("create Timer");
-    const timerId = setInterval(tick, 1000);
-
-    // クリーンアップ関数はコンポーネントがアンマウント、もしくは副作用が再度実行された時に呼ばれる。
-    // ↑で作成したタイマー（timerId）は削除しない限り、コンポーネントがアンマウントされた後も延々と実行され続けてしまう。
-    // そのため、コンポーネントがアンマウント、もしくは副作用が再度実行された時に clearInterval でタイマーを削除する。
-    return () => {
-      console.log("cleanup Timer");
-      clearInterval(timerId);
-    };
-  }, []);
+    // ref オブジェクトが更新されてもコンポーネントは再レンダーされない。
+    prevCountRef.current = count;
+  });
 
   return (
-    <div>
-      <p>time: {timeLeft}</p>
-      <button onClick={reset}>reset</button>
-    </div>
+    <>
+      <p>
+        現在のcount: {count}, 前回のcount: {prevCountRef.current}
+      </p>
+      <p>前回のcountより{prevCountRef.current > count ? '小さい' : '大きい'}</p>
+      <button onClick={() => setCount(Math.floor(Math.random() * 11))}>
+        update
+      </button>
+    </>
   );
 }
-
-function App() {
-  // コンポーネントをレンダーするかどうかのフラグ
-  const [visible, setVisible] = useState(true);
-
-  return (
-    <div>
-      <button onClick={() => setVisible(!visible)}>toggle Timer</button>
-
-      {/* visible が true であれば Timer コンポーネントをレンダーする。
-      false にすれば何もレンダーされない（Timer コンポーネントがアンマウントされる）ため、
-      Timer コンポーネントの console.log('cleanup Timer'); などが実行される。 */}
-      {visible ? <Timer /> : ""}
-    </div>
-  );
-}
-
-export default App;
