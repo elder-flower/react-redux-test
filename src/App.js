44cb5736aca7478042b61c8ce7f6a19d87a3ca96
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-01-29 22:43:35 +0900

    04-Context の更新により不要な再レンダーが発生している例

--- src/App.js
+++ src/App.js
@@ -1,44 +1,64 @@
-import React, { useState, useContext, createContext } from "react";
+import React, { createContext, useContext, useReducer } from 'react';
 import './index.css';
 
-// Context オブジェクト
-const LangContext = createContext();
+const CountContext = createContext();
 
-export default function App() {
-  const [lang, setLang] = useState("JP");
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return { count: state.count + 1 };
+    }
+    case 'DECREMENET': {
+      return { count: state.count - 1 };
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+function CountProvider({ children }) {
+  const [state, dispatch] = useReducer(countReducer, { count: 0 });
+  const value = {
+    state,
+    dispatch
+  };
+
+  return (
+    // value（state か dispatch のどちらか）が更新したら、
+    // CountContext.Provider 内のすべての Consumer が再レンダーされる。
+    <CountContext.Provider value={value}>{children}</CountContext.Provider>
+  );
+}
+
+function Count() {
+  console.log('render Count');
+  // CountContext からは state のみを取得しているが、
+  // dispatch が更新されても再レンダーされる
+  const { state } = useContext(CountContext);
+
+  return <h1>{state.count}</h1>;
+}
+
+function Counter() {
+  console.log('render Counter');
+  // CountContext からは dispatch のみを取得しているが、
+  // state が更新されても再レンダーされる
+  const { dispatch } = useContext(CountContext);
 
   return (
     <>
-      <button onClick={() => setLang("JP")}>日本語</button>
-      <button onClick={() => setLang("EN")}>English</button>
-      <LangContext.Provider value={lang}>
-        <Header />
-        <Body />
-      </LangContext.Provider>
+      <button onClick={() => dispatch({ type: 'DECREMENET' })}>-</button>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
     </>
   );
 }
 
-function Header() {
-  const lang = useContext(LangContext);
-  const text = {
-    JP: "速習 React Hooks",
-    EN: "React Hooks Quick Start Guide"
-  };
-
+export default function App() {
   return (
-    <header>
-      <h1>{text[lang]}</h1>
-    </header>
+    <CountProvider>
+      <Count />
+      <Counter />
+    </CountProvider>
   );
-}
-
-function Body() {
-  const lang = useContext(LangContext);
-  const text = {
-    JP: "useContext の利用例",
-    EN: "useContext Example"
-  };
-
-  return <p>{text[lang]}</p>;
 }
