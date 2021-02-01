b36480f5ca926c0b084f6cb75a7a675a6b5663e2
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-02-01 22:34:03 +0900

    08-props.children を利用して Prop drilling 問題を解消する

--- src/App.js
+++ src/App.js
@@ -1,69 +1,37 @@
-import React, { createContext, useContext, useReducer, useMemo } from 'react';
+import React, { useState } from "react";
 
-const CountContext = createContext();
-
-function countReducer(state, action) {
-  switch (action.type) {
-    case 'INCREMENT': {
-      return { count: state.count + 1 };
-    }
-    case 'DECREMENT': {
-      return { count: state.count - 1 };
-    }
-    default: {
-      return state;
-    }
-  }
-}
-
-function CountProvider({ children }) {
-  const [state, dispatch] = useReducer(countReducer, { count: 0 });
+export default function App() {
+  const [count, setCount] = useState(0);
   const value = {
-    state,
-    dispatch
+    name: "soarflat",
+    handleClick: () => setCount((count) => count + 1)
   };
 
   return (
-    / value（state か dispatch のどちらか）が更新したら、
-    / CountContext.Provider 内のすべての Consumer が再レンダーされる。
-    <CountContext.Provider value={value}>{children}</CountContext.Provider>
+    <div>
+      <p>count: {count}</p>
+      <Child>
+        <GrandChild>
+          <GreatGrandChild value={value} />
+        </GrandChild>
+      </Child>
+    </div>
   );
 }
 
-function Count() {
-  console.log('render Count');
-  / CountContext からは state のみを取得しているが、
-  / dispatch が更新されても再レンダーされる
-  const { state } = useContext(CountContext);
-
-  return <h1>{state.count}</h1>;
+function Child({ children }) {
+  return <>{children}</>;
 }
 
-function Counter() {
-  console.log('render Counter');
-  / CountContext からは dispatch のみを取得しているが、
-  / state が更新されても再レンダーされる
-  const { dispatch } = useContext(CountContext);
-
-  / CountContext.Provider の value の更新による Counter コンポーネントの
-  / 再レンダーは避けられない。そのため dispatch を利用するレンダリング結果（計算結果）を
-  / メモ化し、不要な再レンダーを防ぐ。
-  return useMemo(() => {
-    console.log('rerender Counter');
-    return (
-      <>
-        <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
-        <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
-      </>
-    );
-  }, [dispatch]);
+function GrandChild({ children }) {
+  return <>{children}</>;
 }
 
-export default function App() {
+function GreatGrandChild({ value }) {
   return (
-    <CountProvider>
-      <Count />
-      <Counter />
-    </CountProvider>
+    <>
+      <p>{value.name}</p>
+      <button onClick={value.handleClick}>increment</button>
+    </>
   );
 }
