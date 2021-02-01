b54349e20bafaceb4bb101e113da9bcd4499eba3
elder-flower <loreley@loreleynoMacBook-Pro.local>
2021-02-01 22:27:41 +0900

    08-props.children を利用して Prop drilling 問題を解消する

--- src/App.js
+++ src/App.js
@@ -24,16 +24,16 @@ function CountProvider({ children }) {
   };
 
   return (
-    // value（state か dispatch のどちらか）が更新したら、
-    // CountContext.Provider 内のすべての Consumer が再レンダーされる。
+    / value（state か dispatch のどちらか）が更新したら、
+    / CountContext.Provider 内のすべての Consumer が再レンダーされる。
     <CountContext.Provider value={value}>{children}</CountContext.Provider>
   );
 }
 
 function Count() {
   console.log('render Count');
-  // CountContext からは state のみを取得しているが、
-  // dispatch が更新されても再レンダーされる
+  / CountContext からは state のみを取得しているが、
+  / dispatch が更新されても再レンダーされる
   const { state } = useContext(CountContext);
 
   return <h1>{state.count}</h1>;
@@ -41,13 +41,13 @@ function Count() {
 
 function Counter() {
   console.log('render Counter');
-  // CountContext からは dispatch のみを取得しているが、
-  // state が更新されても再レンダーされる
+  / CountContext からは dispatch のみを取得しているが、
+  / state が更新されても再レンダーされる
   const { dispatch } = useContext(CountContext);
 
-  // CountContext.Provider の value の更新による Counter コンポーネントの
-  // 再レンダーは避けられない。そのため dispatch を利用するレンダリング結果（計算結果）を
-  // メモ化し、不要な再レンダーを防ぐ。
+  / CountContext.Provider の value の更新による Counter コンポーネントの
+  / 再レンダーは避けられない。そのため dispatch を利用するレンダリング結果（計算結果）を
+  / メモ化し、不要な再レンダーを防ぐ。
   return useMemo(() => {
     console.log('rerender Counter');
     return (
